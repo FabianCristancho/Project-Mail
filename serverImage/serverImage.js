ca8666592e2e0cce1d99bae18a44fbf927c5f1af
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-09-18 15:20:21 -0500

    Implemented promises at pixels color map

--- serverImage/serverImage.js
+++ serverImage/serverImage.js
@@ -1,48 +1,39 @@
 var Jimp = require('jimp');
 
-// colors = new Map();
-var count = 0;
+function getColorPixels(inputImage) {
+	return new Promise((resolve, reject) => {
+          var colors = new Map();
+          var count = 0;
+		Jimp.read(inputImage)
+			.then((image) => {
+				image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
+					var auxString = JSON.stringify(Jimp.intToRGBA(image.getPixelColor(x, y)));
+					if (colors.has(auxString)) {
+						count = colors.get(auxString) + 1;
+						colors.set(auxString, count);
+					} else {
+						colors.set(auxString, 1);
+					}
+				});
+                    resolve(colors);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+}
 
-function getColorPixels(inputImage){
-     var colors = new Map();
-     Jimp.read(inputImage)
-     .then(image => {
-     image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
-         var auxString = JSON.stringify(Jimp.intToRGBA(image.getPixelColor(x,y)));
-          if(colors.has(auxString)){
-               count = colors.get(auxString) + 1;
-               colors.set(auxString, count);
-          }else{
-               colors.set(auxString, 1);
+function recorreMap(image) {
+     getColorPixels(image).then((res)=>{
+          pixelsColors = res;
+          for (var [ key, value ] of pixelsColors) {
+               console.log(key + ' : ' + value);
           }
-     });
-     for (var [key, value] of colors){
-          console.log(JSON.stringify(key) +' = ' +value);
-     }
-     // getColorPixels();
-     })
-     .catch(err=>{
+     }).catch(err =>{
           console.log(err);
-     });
-     return colors;
+     })
 }
 
-getColorPixels('test.png');
 
-function recorreMap(){
-     let pixelsColors = getColorPixels('test.png');
-     for (var [key, value] of pixelsColors){
-          console.log(JSON.stringify(key) +' = ' +value);
-     }
-}
-
-// recorreMap();
-
-
-
-
-// function getColorPixels(){
-//      for (var [key, value] of colors){
-//           console.log(JSON.stringify(key) +' = ' +value);
-//      }
-// }
+recorreMap('test.png');
+recorreMap('https://icons.iconarchive.com/icons/graphicloads/android-settings/16/plus-icon.png');
